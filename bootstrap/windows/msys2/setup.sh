836bf71ca15ed3ff8e8fb4973e1f8d760dbd672d
Gabriel Frigo <66432268+GabrielFrigo4@users.noreply.github.com>
2026-07-03 22:57:39 -0300

    Update Vault

--- bootstrap/windows/msys2/setup.sh
+++ bootstrap/windows/msys2/setup.sh
@@ -15,18 +15,7 @@ EOF
 ### ################################
 
 chmod 0600 "${FRIGO_SERVER_KEY}"
-cat << 'EOF' | tee "/usr/local/bin/frigo-server" > "/dev/null"
-#!/bin/sh
-ssh -i "${FRIGO_SERVER_KEY}" "ubuntu@${FRIGO_SERVER_IP}"
-EOF
-chmod +x "/usr/local/bin/frigo-server"
-
 chmod 0600 "${ORBS_SERVER_KEY}"
-cat << 'EOF' | tee "/usr/local/bin/orbs-server" > "/dev/null"
-#!/bin/sh
-ssh -i "${ORBS_SERVER_KEY}" "ubuntu@${ORBS_SERVER_IP}"
-EOF
-chmod +x "/usr/local/bin/orbs-server"
 
 ### ################################################################################################################################
 
